554678ba688bff309cc0cb2a2835dd0c839fefe5
NiclanX <nicardolannaman@gmail.com>
2026-07-06 22:37:23 -0500

    Redirect on log out added

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import supabase from '../services/Supabase'
+import { useContext } from 'react';
+import { sessionContext } from '../services/useGetSession';
+import { useNavigate } from 'react-router-dom';
 
 function Dashboard() {
+
+  const {user} = useContext(sessionContext)
+  const navigate = useNavigate()
+
+async function signout() {
+  const {error} = await supabase.auth.signOut()
+
+  if (error) {
+    console.error(`This is the error: ${error}`);
+    return
+  }
+
+  navigate('/login')
+
+}
+
+
   return (
+    <>
     <div>Dashboard</div>
+    <button onClick={signout}>SIgn Out Now!</button>
+    </>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -13,6 +13,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 
       <App />
     </SessionProvider>
-  
+
   </React.StrictMode>,
 )
